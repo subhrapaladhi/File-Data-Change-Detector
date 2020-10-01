7b49b3a2629ee53b933775958453f6ff28dd3fdb
subhra paladhi <subhrapaladhi9@gmail.com>
2020-10-01 22:10:44 +0530

    hashing file in blocks

--- fileHasher.py
+++ fileHasher.py
@@ -1,3 +1,5 @@
+import hashlib
+
 class FileHasher:
     fileData = ""
     
@@ -9,6 +11,23 @@ class FileHasher:
         self.fileData = file.read()
         file.close()
 
+    def hasher(self):
+        self.hashArray = []
+        
+        start = 0
+        end = min(len(self.fileData),25)
+        
+        while(start<len(self.fileData)):
+            substr = self.fileData[start:end]
+            # print(substr)
+            result = hashlib.sha256(substr).digest()
+            self.hashArray.append(result)
+            start = end
+            end = min(len(self.fileData),end+25)
+        
+        # for i in self.hashArray:
+        #     print(i)
+
 fileHasher = FileHasher("./data.txt")
 fileHasher.readFileData()
-print(fileHasher.fileData)
+fileHasher.hasher()
