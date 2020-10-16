6068521f3aa811eb1b809408eb9431ecc7fa9dc6
subhra paladhi <subhrapaladhi9@gmail.com>
2020-10-16 22:00:13 +0530

    getting list of changes

--- fileDataChecker.py
+++ fileDataChecker.py
@@ -38,9 +38,52 @@ class FileDataChecker:
             start +=1 
             end +=1
             print(substr)
-            print(result)
+            # print(result)
+    
+    def compareData(self):
+        oriLen = len(self.hashArray)
+        modLen = len(self.editedFileHashArray)
+        print("orilen = {} || modlen = {}".format(oriLen,modLen))
+        if(oriLen == modLen):
+            i = 0
+            while(i<oriLen):
+                if(self.hashArray[i] != self.editedFileHashArray[i]):
+                    origData = self.unhashedDataArray[i]
+                    editedData = self.editedFileUnhashedDataArray[i]
+                    print("Original Data: {} VS Edited Data: {}".format(origData,editedData))
+                i+=5
+
+        elif(oriLen > modLen):
+            i = 0
+            while(i<modLen):
+                origData = self.unhashedDataArray[i]
+                editedData = self.editedFileUnhashedDataArray[i]
+                print("initial||| Original Data: {} VS Edited Data: {}".format(origData,editedData))
+                if(self.hashArray[i] != self.editedFileHashArray[i]):
+                    print("!!!! Original Data: {} VS Edited Data: {} !!!!".format(origData,editedData))
+                    i+=5
+                    continue
+                i+=1
+            temp = b" "
+            leftData = temp.join(self.unhashedDataArray[i:])
+            print("This data is left in the edited data: {}".format(leftData))
+
+        else:       # modLen > orilen
+            i = 0
+            while(i<oriLen):
+                if(self.hashArray[i] != self.editedFileHashArray[i]):
+                    origData = self.unhashedDataArray[i]
+                    editedData = self.editedFileUnhashedDataArray[i]
+                    print("Original Data: {} VS Edited Data: {}".format(origData,editedData))
+                i+=5
+            temp = b" "
+            leftData = temp.join(self.unhashedDataArray[i:])
+            print("leftData: {}".format(leftData))
+
+
 
 fileDataChecker = FileDataChecker("./data.txt","803471303176")
 fileDataChecker.readFileData()
 fileDataChecker.getHasherData()
 fileDataChecker.hasher()
+fileDataChecker.compareData()
